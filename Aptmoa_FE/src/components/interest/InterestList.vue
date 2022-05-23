fe85cd1dc679d73a315b13b3cf8d290527b9df0f
mingggkeee <mksu234@naver.com>
2022-05-23 22:12:47 +0900

    비로그인 접근 제한, 관심목록조회 appbar생성

---
+++ Aptmoa_FE/src/components/interest/InterestList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "AptMoaInterestList",
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {}
+};
+</script>
+
+<style scoped></style>
